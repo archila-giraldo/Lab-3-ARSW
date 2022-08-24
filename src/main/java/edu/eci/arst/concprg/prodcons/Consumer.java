11af3c2a74fb97a9fe8134cb26795f1a939618c1
luis.giraldo-r <luis>
2022-08-24 09:55:29 -0500

    PARTE I

---
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread {
+
+    private Queue<Integer> queue;
+
+
+    public Consumer(Queue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (queue) {
+                if (queue.size() > 0) {
+                    int elem = queue.poll();
+                    System.out.println("Consumer consumes " + elem);
+                    queue.notifyAll();
+                } else {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+
+            //reducir cpu vamos a añadir un tiempo de espera
+            try {
+                this.sleep(1000);
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
+
+
+}
---
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import sun.awt.image.IntegerComponentRaster;
+import sun.util.resources.ga.LocaleNames_ga;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StartProduction {
+    
+    
+    public static void main(String[] args) {
+        
+        Queue<Integer>  queue=new LinkedBlockingQueue<>();
+
+
+
+        new Producer(queue, 10).start();
+        
+        //let the producer create products for 5 seconds (stock).
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(StartProduction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        new Consumer(queue).start();
+    }
+    
+
+}
